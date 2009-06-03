7822b1bb57bbbe539f8368c1a773616611560e82
松島 浩道 <hylom@Macintosh.local>
2009-06-04 01:37:09 +0900

    add plugin.py

---
+++ fusuma/plugins/plugin.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# This file is part of Fusuma website management system.
+#
+# Copyright (c) hylom <hylomm at gmail.com>, 2008.
+# 
+# This file is released under the GPL.
+#
+
+def define_config(key, default_param):
+    """
+    Define configuration.
+
+    @param key: key name
+    @type key: string
+
+    @param default_param: key's default value.
+    @type default_param: string, int, ...
+    """
+
